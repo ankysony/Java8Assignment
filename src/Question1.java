46b031a3c3ab61faf7d639f9f0d928ce5afa7482
Ankit Soni <ankisoni453@gmail.com>
2023-05-19 15:58:33 +0530

    add

--- src/Question1.java
+++ src/Question1.java
@@ -1,6 +1,16 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Question1 {
 
     public static void main(String[] args) {
+        List<String> stringList = Arrays.asList("Hello", "World", "Stream", "API");
 
+        String concatenatedString = stringList.stream()
+                .collect(Collectors.joining());
+
+        System.out.println(concatenatedString);
     }
 }
